81b80c148400a5b9b1db5e9296cdd19ca3203f12
Sultanbek02173 <sultan.02173@gmail.com>
2025-05-09 16:17:57 +0600

    rename scss

--- src/features/cardAfisha/CardAfisha.jsx
+++ src/features/cardAfisha/CardAfisha.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
-import "./CardAfisha.scss";
+import "./cardAfisha.scss";
 
 export const CardAfisha = ({ id, image, title, time, description, tablet }) => {
   return (
